8f77b718d96b454fb394c3397955f25eb9b9a526
NOA1234noah <noashuker100@gmail.com>
2023-06-08 15:01:47 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
 import './App.css';
-import Counter from './commponents/counter';
 import Hw1 from './commponents/hw1';
-import InputVal from './commponents/inputVal';
-import Message from './commponents/message';
 
 function App() {
   return (
     <div className="container">
-<Hw1/>
+      <Hw1 />
     </div>
   );
 }
