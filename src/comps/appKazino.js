7fa5c29885e366f5f82670f669ce6b9e9d5d9d4f
NOA1234noah <noashuker100@gmail.com>
2023-06-15 11:03:27 +0300

    jnb

--- src/comps/appKazino.js
+++ src/comps/appKazino.js
@@ -3,13 +3,21 @@ import { useRef } from 'react';
 import { useEffect } from 'react';
 
 export default function AppKazino() {
-    const [ranNum, setRand] = useState();
+    const [ranNum, setRand] = useState(1);
     const [coinNum, setCoin] = useState(5);
-    const [message, setMessage] = useState(5);
+    const [message, setMessage] = useState();
+    useEffect(() => {
+        if (localStorage["coins"]) {
+            setCoin(Number(localStorage["coins"]))
+        }
+        else {
+            localStorage.setItem("coins", 5);
+        }
+    }, [])
     const roll = () => {
-        let rnd = Math.random()*6;
+        let rnd = Math.random() * 6;
         let rand = Math.ceil(rnd);
-        
+
         setRand(rand);
         if (rand == 5 || rand == 6) {
             setCoin(coinNum + 2);
@@ -22,15 +30,17 @@ export default function AppKazino() {
         if (coinNum == 0) {
             alert("sorry!!\n no enught money");
             setCoin(5);
+            setMessage("");
         }
+        localStorage.setItem("coins", coinNum);
     }
     return (
         <div className='text-center'><h1 className='text-danger'>Kazino</h1>
-            <p>your coins: {coinNum}</p>
-            <p>your num random {ranNum}</p>
-              <img src={`./dice_images/dice${ranNum}.jpg`} /><br/>
-              <strong className={(message=="win")?"text-success":"text-danger"}>{message}</strong>
-          <br/>
+            <h2>your coins: {coinNum}</h2>
+            <h2>your num random {ranNum}</h2>
+            <img src={`./dice_images/dice${ranNum}.jpg`} /><br />
+            <h2 className={(message == "win") ? "text-success" : "text-danger"}>{message}</h2>
+            <br />
             <button className='btn btn-danger p-3 rounded-5' onClick={roll}>  Roll  </button>
         </div>
     )
